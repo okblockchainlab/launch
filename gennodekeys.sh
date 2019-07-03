66d28664c78abb76761599ba394d7be1557f9be5
zhongqiuwood <zhongqiuwood@gmail.com>
2019-07-04 07:13:21 +0800

    add node keys

---
+++ gennodekeys.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+for ((index=0; index<3; index++)) do
+    okchaind tendermint show-node-id --home nodekey/seed${index}
+    okchaind tendermint show-validator --home nodekey/seed${index}
+
+done
+
+for ((index=0; index<$1; index++)) do
+
+    okchaind tendermint show-node-id --home nodekey/node${index}
+    okchaind tendermint show-validator --home nodekey/node${index}
+
+done
+
+
