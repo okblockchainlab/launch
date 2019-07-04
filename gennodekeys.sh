6cd951c878b3e0c6f4181b74cfe568096c24828c
zhongqiuwood <zhongqiuwood@gmail.com>
2019-07-04 08:41:28 +0800

    seed id

--- gennodekeys.sh
+++ gennodekeys.sh
@@ -2,16 +2,15 @@
 
 
 for ((index=0; index<3; index++)) do
+    echo "seed $index"
     okchaind tendermint show-node-id --home nodekey/seed${index}
     okchaind tendermint show-validator --home nodekey/seed${index}
-
 done
 
 for ((index=0; index<$1; index++)) do
-
     okchaind tendermint show-node-id --home nodekey/node${index}
     okchaind tendermint show-validator --home nodekey/node${index}
-
 done
+
 
 
