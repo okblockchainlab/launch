970aa10cb7d325cf90ee87f854d8eef688ef1fad
banishee <yili.ge@okcoin.net>
2019-07-10 13:35:58 +0800

    revert codes in downloadGaia.sh

--- systemctl/remote_launch/downloadGaia.sh
+++ systemctl/remote_launch/downloadGaia.sh
@@ -5,14 +5,8 @@
 function downloadGaia {
     echo "====================== download gaia bins ${1} ======================"
     cosmosbinpath=/root/cosmos/cosmosbins
-   if [[ ! -d ${cosmosbinpath} ]]; then
-        mkdir -p ${cosmosbinpath}
-        git clone -b ${1} ${COSMOS_BINS_GIT} ${cosmosbinpath}
-   else
-        cd ${cosmosbinpath}
-        git checkout -b ${1}
-        git pull origin ${1}
-   fi
+    rm -rf ${cosmosbinpath}
+    git clone -b ${1} ${COSMOS_BINS_GIT} ${cosmosbinpath}
 
     cp -f ${cosmosbinpath}/gaiad ${GOBIN}
     cp -f ${cosmosbinpath}/gaiacli ${GOBIN}
