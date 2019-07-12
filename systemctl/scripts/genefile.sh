f4f9d35ed9a7d313887bc5c686a822258d550eb1
mengxiangjian <mengxiangjian888@gmail.com>
2019-07-12 17:26:29 +0800

    update genefile.sh

--- systemctl/scripts/genefile.sh
+++ systemctl/scripts/genefile.sh
@@ -17,7 +17,7 @@ do
     ${OKCHAIN_CLI} keys add --recover captain --home ${home_cli} -y -m "${CAPTAIN_MNEMONIC}"
 
     ${OKCHAIN_DAEMON} init --chain-id okchain --home ${home_d}
-    
+
     ${OKCHAIN_CLI} config chain-id okchain --home ${home_cli}
     ${OKCHAIN_CLI} config trust-node true --home ${home_cli}
     ${OKCHAIN_CLI} config indent true --home ${home_cli}
@@ -42,17 +42,18 @@ produceGenesis(){
 init_validator_node_home_dir() {
 
     rm ${OKCHAIN_LAUNCH_TOP}/gentx/data/gentx-*
-
+    accounts=""
     echo "===== init validator node ====="
     for ((i=0; i<${#OKCHAIN_TESTNET_VAL_NODES[@]}; i++))
     do
         host=${HOSTS_PREFIX}${OKCHAIN_TESTNET_VAL_NODES[i]}
-        mnemonic=${OKCHAIN_TESTNET_VAL_ADMIN_MNEMONIC[i]}
         home_d=${HOME_DAEMON}/${host}
         home_cli=${HOME_CLI}/${host}
+        mnemonic=${OKCHAIN_TESTNET_VAL_ADMIN_MNEMONIC[i]}
 
         ${OKCHAIN_CLI} keys add --recover captain --home ${home_cli} -y -m "${CAPTAIN_MNEMONIC}"
         ${OKCHAIN_CLI} keys add --recover ${host} --home ${home_cli}  -y -m "${mnemonic}"
+        accounts=${accounts}\"$(${OKCHAIN_CLI} keys show ${host} -a --home ${home_cli})\"": 1000,"
 
         ${OKCHAIN_DAEMON} init --chain-id okchain --home ${home_d}
 
@@ -63,15 +64,18 @@ init_validator_node_home_dir() {
         # Adds an account into the ~/.okchaind/config/genesis.json
         ${OKCHAIN_DAEMON} add-genesis-account $(${OKCHAIN_CLI} keys show ${host} -a --home ${home_cli}) \
             1okb --home ${home_d}
-
-        # Genesis transaction written to "~/.okchaind/config/gentx/gentx-e6bb1b79ebe58471d13155c0ceef885b98bb0f64.json"
-        # It creates a genesis piece carrying a self delegation for the account above
         ${OKCHAIN_DAEMON} gentx --amount 1okb --min-self-delegation 1 --commission-rate 0.1 \
             --commission-max-rate 0.5 --commission-max-change-rate 0.001 \
             --pubkey $(${OKCHAIN_DAEMON} tendermint show-validator --home ${home_d}) \
+            --moniker ${host} \
+            --identity "logo|||http://b-ssl.duitang.com/uploads/item/201708/18/20170818203754_MfC5i.jpeg" \
             --name ${host} --home ${home_d} --home-client ${home_cli}
         cp ${home_d}/config/gentx/gentx-*.json ${OKCHAIN_LAUNCH_TOP}/gentx/data
     done
+
+    cat>${OKCHAIN_LAUNCH_TOP}/accounts/others.json<<EOF
+{${accounts%?}}
+EOF
 
     # produce genesis file which ships all the gentx in ${OKCHAIN_LAUNCH_TOP}/gentx/data
     produceGenesis
