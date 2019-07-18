dea1e7bb0304f529b18f9b31a4e5b3fc3839412c
banishee <yili.ge@okcoin.net>
2019-07-18 17:06:05 +0800

    more function

--- remote_launch/mac_shell/generateLocal.sh
+++ remote_launch/mac_shell/generateLocal.sh
@@ -11,24 +11,22 @@ gaiad testnet --v 4 --output-dir $HOME/testnet --chain-id testchain --starting-i
 12345678
 EOF
 
-scp -r $HOME/testnet/node1/ root@okchain22:/root/gaianode
-scp -r $HOME/testnet/node2/ root@okchain23:/root/gaianode
-scp -r $HOME/testnet/node3/ root@okchain24:/root/gaianode
+
+OS=`uname`
+for i in $(seq 0 4);
+do
+    if [ "Linux" = ${OS} ]; then
+       sed -i "s/addr_book_strict = true/addr_book_strict = false/g" $HOME/testnet/node${i}/gaiad/config/config.toml
+    else
+       sed -i '' "s/addr_book_strict = true/addr_book_strict = false/g" $HOME/testnet/node${i}/gaiad/config/config.toml
+   fi
+done
+
+scp -r $HOME/testnet/node1/ root@okchain22:/root/gaianode1
+scp -r $HOME/testnet/node2/ root@okchain23:/root/gaianode2
+scp -r $HOME/testnet/node3/ root@okchain24:/root/gaianode3
 
 #for (( i = 0; i < ${5}; ++i )); do
 #    k=` grep  -n "persistent_peers" node${1}/gaiad/config/config.toml  | cut  -d  ":"  -f  1`
 #    sed -i
-#done
-
-#OS=`uname`
-#for i in $(seq 0 4);
-#do
-#    if [ "Linux" = ${OS} ]; then
-#        sed -i "s/2665/1665/g" gentxs/node${i}.json
-#        sed -i "s/2665/1665/g" node${i}/gaiad/config/config.toml
-#        sed -i "s/2665/1665/g" node${i}/gaiad/config/genesis.json
-#    else
-#        sed -i '' "s/2665/1665/g" node${i}/gaiad/config/config.toml
-#        sed -i '' "s/2665/1665/g" node${i}/gaiad/config/genesis.json
-#    fi
 #done
