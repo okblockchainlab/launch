54cfa48cc7e8759092150c2d4868522ae391524c
banishee <yili.ge@okcoin.net>
2019-07-18 16:17:13 +0800

    add flag /Users/green

--- remote_launch/mac_shell/generateLocal.sh
+++ remote_launch/mac_shell/generateLocal.sh
@@ -1,19 +1,19 @@
 #!/bin bash
 
-rm -rf ../../testnet
+rm -rf $HOME/testnet
 
 gaiad version
 
-gaiad testnet --v 4 --output-dir ../testnet --chain-id testchain --starting-ip-address 192.168.13.121<<EOF
+gaiad testnet --v 4 --output-dir $HOME/testnet --chain-id testchain --starting-ip-address 192.168.13.121<<EOF
 12345678
 12345678
 12345678
 12345678
 EOF
 
-scp -r /root/testnet/node1/ root@okchain22:/root/gaianode
-scp -r /root/testnet/node2/ root@okchain23:/root/gaianode
-scp -r /root/testnet/node3/ root@okchain24:/root/gaianode
+scp -r $HOME/testnet/node1/ root@okchain22:/root/gaianode
+scp -r $HOME/testnet/node2/ root@okchain23:/root/gaianode
+scp -r $HOME/testnet/node3/ root@okchain24:/root/gaianode
 
 #for (( i = 0; i < ${5}; ++i )); do
 #    k=` grep  -n "persistent_peers" node${1}/gaiad/config/config.toml  | cut  -d  ":"  -f  1`
